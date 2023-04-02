7c6c2a83cac8b477e5a6f5d78a594ab2ae849d5c
HrubyVaclav <86952465+HrubyVaclav@users.noreply.github.com>
2023-04-02 11:28:40 +0200

    initial setup

---
+++ src/main/java/cz/upce/nnpia_semestralni_prace/NnpiaSemestralniPraceApplication.java
@@ -0,0 +1,13 @@
+package cz.upce.nnpia_semestralni_prace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnpiaSemestralniPraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnpiaSemestralniPraceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/upce/nnpia_semestralni_prace/NnpiaSemestralniPraceApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.upce.nnpia_semestralni_prace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnpiaSemestralniPraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
